5f3b7c2a50f1e60a89b552edc1e56e0aa11d98fd
FalconGames <falcongames109@gmail.com>
2013-08-07 19:23:07 -0400

    Test commit

--- BukkitCollab/src/io/github/zaboon/BukkitCollab/Main.java
+++ BukkitCollab/src/io/github/zaboon/BukkitCollab/Main.java
@@ -1,10 +1,12 @@
 package io.github.zaboon.BukkitCollab;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 public class Main extends JavaPlugin {
 
 	public void onEnable() {
 
-		System.out.println("Enabling!");
+		System.out.println("Enabling...");
 
 	}
 
