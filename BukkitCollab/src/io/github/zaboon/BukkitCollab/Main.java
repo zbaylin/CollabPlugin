3fa0c5bdb2f2a60805ab13adaf2ce8dd2942d263
FalconGames <falcongames109@gmail.com>
2013-08-07 19:09:59 -0400

    Update Main.java

--- BukkitCollab/src/io/github/zaboon/BukkitCollab/Main.java
+++ BukkitCollab/src/io/github/zaboon/BukkitCollab/Main.java
@@ -1,5 +1,11 @@
 package io.github.zaboon.BukkitCollab;
 
-public class Main {
+public class Main extends JavaPlugin{
+
+  public void onEnable(){
+    
+    System.out.println("Enabling!");
+    
+  }
 
 }
